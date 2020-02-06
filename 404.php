46fc2da9b97713578fe002238060857f7afcc472
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 13:59:03 +0100

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<base href="./">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<title>Lapo | 404 page-not-found User</title>
+	<?php require 'zenq/ui/css.php';?>
+</head>
+<body class="app flex-row align-items-center">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-6">
+				<div class="clearfix">
+					<h1 class="float-left display-3 mr-4">404</h1>
+					<h4 class="pt-3">Oops! You're lost.</h4>
+					<p class="text-muted">The page you are looking for was not found.</p>
+				</div>
+				<div class="input-prepend input-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text">
+							<i class="fa fa-search"></i>
+						</span>
+					</div>
+					<input class="form-control" id="prependedInput" size="16" type="text" placeholder="What are you looking for?">
+					<span class="input-group-append">
+						<button class="btn btn-info" type="button">Search</button>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php require 'zenq/ui/js.php';?>
+
+</body>
+</html>
