4ce76f029f1283e63412cc9a21343584d1a7150c
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 14:00:52 +0100

    Create header.php

---
+++ zenq/ui/header.php
@@ -0,0 +1,54 @@
+<header class="app-header navbar">
+	<button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand" href="#">
+		<img class="navbar-brand-full" src="assets/img/brand/logo_2.jpeg" width="115" height="55px" alt="CoreUI Logo">
+		<img class="navbar-brand-minimized" src="assets/img/brand/logo_2.jpeg" width="115" height="55px" alt="CoreUI Logo">
+	</a>
+	<button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<ul class="nav navbar-nav d-md-down-none">
+		<li class="nav-item px-3">
+			<a class="nav-link" href="#">Dashboard</a>
+		</li>
+
+		<li class="nav-item px-3">
+			<a class="nav-link" href="#">Account</a>
+		</li>
+	</ul>
+	<ul class="nav navbar-nav ml-auto">
+		<li class="nav-item dropdown">
+			<a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+				<i class="fa fa-bell-o"></i>
+				<span class="badge badge-pill badge-danger">5</span>
+			</a>
+			<div class="dropdown-menu dropdown-menu-right">
+				<div class="dropdown-header text-center">
+					<strong>Notification Alert</strong>
+				</div>
+				<a class="dropdown-item" href="#">
+					<i class="fa fa-bell-o"></i> Updates
+					<span class="badge badge-info">42</span>
+				</a>
+			</div>
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+				<img class="img-avatar" src="assets/img/avatars/6.jpg" alt="">
+			</a>
+			<div class="dropdown-menu dropdown-menu-right">
+				<div class="dropdown-header text-center">
+					<strong>Account</strong>
+				</div>
+				<a class="dropdown-item" href="view_account.html">
+					<i class="fa fa-user"></i> View Profile
+				</a>
+				<a class="dropdown-item" href="login.html">
+					<i class="fa fa-lock"></i> Logout
+				</a>
+			</div>
+		</li>
+	</ul>
+</header>
