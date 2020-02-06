7269beec387aa78e71f67b3c48491925aa4a7cb7
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 14:01:37 +0100

    Create transactions.php

---
+++ zenq/view/transactions.php
@@ -0,0 +1,99 @@
+        <main class="main">
+        	<!-- Breadcrumb-->
+        	<ol class="breadcrumb">
+        		<li class="breadcrumb-item">Home</li>
+        		<li class="breadcrumb-item active">Transation</li>
+        		<!-- Breadcrumb Menu-->
+        		<li class="breadcrumb-menu d-md-down-none">
+        			<div class="btn-group" role="group" aria-label="Button group">
+        				<a class="btn" href="#">
+        					<i class="icon-speech"></i>
+        				</a>
+        				<a class="btn" href="./">
+        					<i class="icon-graph"></i>  Dashboard</a>
+        					<a class="btn" href="#">
+        						<i class="icon-settings"></i>  View All Transation</a>
+        					</div>
+        				</li>
+        			</ol>
+        			<div class="container-fluid">
+        				<div class="animated fadeIn">
+        					<div class="row">
+        						<div class="col-sm-12">
+        							<div class="card text-white bg-white">
+        								<div class="card-body">
+        									<input class="form-control" id="company" type="text" placeholder="Search for...">
+        									<input type="submit" value="Search" class="mt-3 btn btn-success" />
+        								</div>
+        							</div>
+        						</div>
+        						<div class="col-sm-12">
+        							<div class="card">
+        								<div class="card-header">
+        									<i class="fa fa-align-justify"></i> View all Transation</div>
+        									<div class="card-body">
+        										<table class="table table-responsive-sm table-striped">
+        											<thead>
+        												<tr>
+        													<th>Account Name</th>
+        													<th>Account No</th>
+        													<th>Transation Type</th>
+        													<th>Amount</th>
+        													<th>Date</th>
+        												</tr>
+        											</thead>
+        											<tbody>
+        												<tr>
+        													<td>James Osagie</td>
+        													<td>121112211</td>
+        													<td><span class="badge badge-success">Savings</span></td>
+        													<td>50,000.00 NGN</td>
+        													<td>22/2/2020</td>
+
+        												</tr>
+        												<tr>
+        													<td>Jeninfer Santos</td>
+        													<td>3233288265</td>
+        													<td><span class="badge badge-danger">Withdrawal</span></td>
+        													<td>30,000.00 NGN</td>
+        													<td>20/20/2020</td>
+
+        												</tr>
+        												<tr>
+        													<td>Cratos Pascal </td>
+        													<td>4883777237</td>
+        													<td><span class="badge badge-primary">Loans Repayment</span></td>
+        													<td>5,000.00 NGN</td>
+        													<td>1/2/2019</td>
+
+        												</tr>
+        											</tbody>
+        										</table>
+        										<ul class="pagination">
+        											<li class="page-item">
+        												<a class="page-link" href="#">Prev</a>
+        											</li>
+        											<li class="page-item active">
+        												<a class="page-link" href="#">1</a>
+        											</li>
+        											<li class="page-item">
+        												<a class="page-link" href="#">2</a>
+        											</li>
+        											<li class="page-item">
+        												<a class="page-link" href="#">3</a>
+        											</li>
+        											<li class="page-item">
+        												<a class="page-link" href="#">4</a>
+        											</li>
+        											<li class="page-item">
+        												<a class="page-link" href="#">Next</a>
+        											</li>
+        										</ul>
+        									</div>
+        								</div>
+        							</div>
+
+        						</div>
+        					</div>
+        				</div>
+        			</main>
