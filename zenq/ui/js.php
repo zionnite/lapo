34c5f38d2fc2dc02280e89f3588d72c0e077f91f
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 14:01:10 +0100

    Create js.php

---
+++ zenq/ui/js.php
@@ -0,0 +1,6 @@
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/pace.min.js"></script>
+	<script src="assets/js/perfect-scrollbar.min.js"></script>
+	<script src="assets/js/coreui.min.js"></script>
