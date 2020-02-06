73e5111638ad0aeec607fc287cc6e04618658b09
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 14:01:26 +0100

    Create member.php

---
+++ zenq/view/member.php
@@ -0,0 +1,57 @@
+<main class="main">
+	<!-- Breadcrumb-->
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item">Home</li>
+
+		<li class="breadcrumb-item active">Profile</li>
+		<!-- Breadcrumb Menu-->
+		<li class="breadcrumb-menu d-md-down-none">
+			<div class="btn-group" role="group" aria-label="Button group">
+				<a class="btn" href="#">
+					<i class="icon-speech"></i>
+				</a>
+				<a class="btn" href="./">
+					<i class="icon-graph"></i>  Dashboard</a>
+					<a class="btn" href="#">
+						<i class="icon-settings"></i>  View Profile</a>
+					</div>
+				</li>
+			</ol>
+			<div class="container-fluid">
+				<div class="animated fadeIn">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="card">
+								<div class="card-header">
+									<i class="fa fa-align-justify"></i> Viewing your Profile</div>
+									<div class="card-body">
+										<div class="row">
+											<div class="col-sm-2">
+												<img src="assets/img/avatars/8.jpg" class="img-circle" style="width:150px; height: 150px;" />
+											</div>
+											<div class="col-sm-6" class="mt-10">
+												<div>
+													<h6>First Name: <span></span></h6>
+													<h6>Last Name: <span></span></h6>
+													<h6>Email Address: <span></span></h6>
+													<h6>Phone Number: <span></span></h6>
+													<h6>Gender: <span></span></h6>
+													<h6>Date Of Birth: <span></span></h6>
+													<hr />
+													<h3 style="color:red;">Account Details</h3>
+													<h6>Account Name: <span></span></h6>
+													<h6>Account Number: <span></span></h6>
+													<h6>Account Balance: <span></span></h6>
+
+													<a href="view_transaction.html" class="btn btn-danger btn-block">Links to All Transaction</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</main>
