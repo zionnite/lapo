c018de474da6026c43374524b7a8f6c29ab68802
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 14:01:30 +0100

    Create members.php

---
+++ zenq/view/members.php
@@ -0,0 +1,119 @@
+                  <main class="main">
+                  	<!-- Breadcrumb-->
+                  	<ol class="breadcrumb">
+                  		<li class="breadcrumb-item">Home</li>
+                  		<li class="breadcrumb-item active">Dashboard</li>
+                  		<!-- Breadcrumb Menu-->
+                  		<li class="breadcrumb-menu d-md-down-none">
+                  			<div class="btn-group" role="group" aria-label="Button group">
+                  				<a class="btn" href="#">
+                  					<i class="icon-speech"></i>
+                  				</a>
+                  				<a class="btn" href="./">
+                  					<i class="icon-graph"></i>  Dashboard</a>
+                  					<a class="btn" href="#">
+                  						<i class="icon-settings"></i>  Viewing All User</a>
+                  					</div>
+                  				</li>
+                  			</ol>
+                  			<div class="container-fluid">
+                  				<div class="animated fadeIn">
+                  					<div class="row">
+                  						<div class="col-sm-12">
+                  							<div class="card text-white bg-white">
+                  								<div class="card-body">
+                  									<input class="form-control" id="company" type="text" placeholder="Search for...">
+                  									<input type="submit" value="Search" class="mt-3 btn btn-success" />
+                  								</div>
+                  							</div>
+                  						</div>
+                  						<div class="col-sm-12">
+                  							<div class="card">
+                  								<div class="card-header">
+                  									<i class="fa fa-align-justify"></i> Showing all User</div>
+                  									<div class="card-body">
+                  										<table class="table table-responsive-sm table-striped">
+                  											<thead>
+                  												<tr>
+                  													<th>ID</th>
+                  													<th>First Name</th>
+                  													<th>Last Name</th>
+                  													<th>Email Address</th>
+                  													<th>Phone Number</th>
+                  													<th>Gender</th>
+                  													<th>D.O.B</th>
+                  													<th></th>
+                  												</tr>
+                  											</thead>
+                  											<tbody>
+                  												<tr>
+                  													<td>122109911 </td>
+                  													<td>James </td>
+                  													<td>Osital</td>
+                  													<td>email@email.com</td>
+                  													<td>1234xxxx22</td>
+                  													<td><span class="badge badge-success">Male</span></td>
+                  													<td>12/3/1990</td>
+                  													<td>
+                  														<div class="dropdown">
+                  															<a class="btn btn-danger dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  																Action
+                  															</a>
+                  															<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  																<a class="dropdown-item" href="#">View Savings</a>
+                  																<a class="dropdown-item" href="#">View Loans</a>
+                  																<a class="dropdown-item" href="#">View Profile</a>
+                  															</div>
+                  														</div>
+                  													</td>
+                  												</tr>
+                  												<tr>
+                  													<td>112340009 </td>
+                  													<td>Jeninfer </td>
+                  													<td>Pascal</td>
+                  													<td>pasacal@email.com</td>
+                  													<td>1234xxxx22</td>
+                  													<td><span class="badge badge-danger">Female</span></td>
+                  													<td>12/3/1991</td>
+                  													<td>
+                  														<div class="dropdown">
+                  															<a class="btn btn-danger dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  																Action
+                  															</a>
+                  															<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  																<a class="dropdown-item" href="#">View Savings</a>
+                  																<a class="dropdown-item" href="#">View Loans</a>
+                  																<a class="dropdown-item" href="#">View Profile</a>
+                  															</div>
+                  														</div>
+                  													</td>
+                  												</tr>
+                  											</tbody>
+                  										</table>
+                  										<ul class="pagination">
+                  											<li class="page-item">
+                  												<a class="page-link" href="#">Prev</a>
+                  											</li>
+                  											<li class="page-item active">
+                  												<a class="page-link" href="#">1</a>
+                  											</li>
+                  											<li class="page-item">
+                  												<a class="page-link" href="#">2</a>
+                  											</li>
+                  											<li class="page-item">
+                  												<a class="page-link" href="#">3</a>
+                  											</li>
+                  											<li class="page-item">
+                  												<a class="page-link" href="#">4</a>
+                  											</li>
+                  											<li class="page-item">
+                  												<a class="page-link" href="#">Next</a>
+                  											</li>
+                  										</ul>
+                  									</div>
+                  								</div>
+                  							</div>
+                  						</div>
+                  					</div>
+                  				</div>
+                  			</main>
