07e691a5fc96a84d1b6581774c1d4af5152e7fd5
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 14:00:48 +0100

    Create css.php

---
+++ zenq/ui/css.php
@@ -0,0 +1,9 @@
+	<!-- Icons-->
+	<link rel="icon" type="image/ico" href="./img/favicon.ico" sizes="any">
+	<link href="assets/css/coreui-icons.min.css" rel="stylesheet">
+	<link href="assets/css/flag-icon.min.css" rel="stylesheet">
+	<!-- Main styles for this application-->
+	<link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/vendors/pace-progress/css/pace.min.css" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/5f98ed3b44.js" crossorigin="anonymous"></script>
+	<link href="assets/css/zenq.css" rel="stylesheet">
