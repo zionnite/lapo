f374056528ac2d3a52ab7b3d8fb6d7074412c7da
Anthony O. Osawere <anthony@osawere.com>
2020-02-06 14:01:14 +0100

    Create footer.php

---
+++ zenq/ui/footer.php
@@ -0,0 +1,9 @@
+<footer class="app-footer">
+	<div>
+		<span>&copy; 2020 Lapo.</span>
+	</div>
+	<div class="ml-auto">
+		<span>Powered by</span>
+		<img src="assets/img/paymedix.jpg" style="height: 24px;">
+	</div>
+</footer>
