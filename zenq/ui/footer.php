a1078a1e32a4f81a89b6828b28b58b2be7c5d71e
Anthony O. Osawere <anthony@osawere.com>
2020-02-07 10:32:31 +0100

    Update

--- zenq/ui/footer.php
+++ zenq/ui/footer.php
@@ -1,9 +1,8 @@
 <footer class="app-footer">
 	<div>
-		<span>&copy; 2020 Lapo.</span>
-	</div>
-	<div class="ml-auto">
-		<span>Powered by</span>
-		<img src="assets/img/paymedix.jpg" style="height: 24px;">
-	</div>
-</footer>
+		<span>&copy; 2020 Lapo.</span>	</div>
+		<div class="ml-auto">
+			<span>Powered by</span>
+			<img src="assets/img/paymedix.jpg" style="height: 24px;">
+		</div>
+	</footer>
