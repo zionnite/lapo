be4dc097fb6e97ea182ba348b65ecf173c031d40
Anthony O. Osawere <anthony@osawere.com>
2020-02-07 11:33:55 +0100

    Update login.php

--- login.php
+++ login.php
@@ -15,32 +15,32 @@
 				<div class="card-group">
 					<div class="card p-4">
 						<div class="card-body">
-							<h1>Login</h1>
-							<p class="text-muted">Sign In to your account</p>
-							<div class="input-group mb-3">
-								<div class="input-group-prepend">
-									<span class="input-group-text">
-										<i class="icon-user"></i>
-									</span>
+							<form name="loginForm" id="loginForm" method="POST" action="go.php">
+								<h1>Login</h1>
+								<p class="text-muted">Sign In to your account</p>
+								<div class="input-group mb-3">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="icon-user"></i></span>
+									</div>
+									<input class="form-control" type="text" placeholder="Username" name="UserID">
 								</div>
-								<input class="form-control" type="text" placeholder="Username">
-							</div>
-							<div class="input-group mb-4">
-								<div class="input-group-prepend">
-									<span class="input-group-text">
-										<i class="icon-lock"></i>
-									</span>
+								<div class="input-group mb-4">
+									<div class="input-group-prepend">
+										<span class="input-group-text">
+											<i class="icon-lock"></i>
+										</span>
+									</div>
+									<input class="form-control" type="password" placeholder="Password" name="Password">
 								</div>
-								<input class="form-control" type="password" placeholder="Password">
-							</div>
-							<div class="row">
-								<div class="col-6">
-									<button class="btn btn-primary px-4" type="button">Login</button>
+								<div class="row">
+									<div class="col-6">
+										<button class="btn btn-primary px-4" type="submit">Login</button>
+									</div>
+									<div class="col-6 text-right">
+										<button class="btn btn-link px-0" type="button">Forgot password?</button>
+									</div>
 								</div>
-								<div class="col-6 text-right">
-									<button class="btn btn-link px-0" type="button">Forgot password?</button>
-								</div>
-							</div>
+							</form>
 						</div>
 					</div>
 					<div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
