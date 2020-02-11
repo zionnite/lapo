1d4b29f5edd799def72490525bbaa305f4928a24
Anthony O. Osawere <anthony@osawere.com>
2020-02-11 07:48:33 +0100

    Update method.php

--- zenq/libry/method.php
+++ zenq/libry/method.php
@@ -15,5 +15,20 @@ class oMethod {
 		}
 		return FALSE;
 	}
+
+	public static function isGet($var=''){
+		//check if method is post
+		if(empty($var)){
+			if(!empty($_GET)){return TRUE;}
+		}
+		else {
+			//check if post variable exist and return value otherwise return empty
+			if(!empty($_GET[$var])){
+				return trim($_GET[$var]);
+			}
+			elseif(isset($_GET[$var])){return '';}
+		}
+		return FALSE;
+	}
 }
 ?>
