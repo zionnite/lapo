03e69942b4d65a1b56c32a2052b329c5d2c645a1
Anthony O. Osawere <anthony@osawere.com>
2020-02-11 07:16:06 +0100

    Update importDB.sql

--- _workshop/importDB.sql
+++ _workshop/importDB.sql
@@ -1,9 +1,3 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,15 +19,9 @@ CREATE TABLE IF NOT EXISTS `user` (
   KEY `BIND` (`BIND`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
 REPLACE INTO `user` (`ID`, `BIND`, `MemberID`, `Username`, `Password`, `Status`, `Type`, `FirstName`, `LastName`, `Email`, `Phone`, `Gender`, `DoB`, `Savings`, `Loans`) VALUES
 	(1, '01012012', NULL, 'admin', 'admin', 2, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 	(2, 'P78173466', '121', NULL, NULL, 1, 1, 'John', 'Snow', 'john@snow.com', '09038877672', 'Male', '1950-11-29', 0, 0),
 	(3, 'F71981370', '434', NULL, NULL, 1, 1, 'Richard', 'Priar', 'rich@priar.com', '09038877953', 'Male', '1950-11-16', 0, 0),
 	(4, 'U54497406', '434', NULL, NULL, 1, 1, 'Zuric', 'Priar', 'zuric@priar.com', '09038877956', 'Male', '1950-04-16', 0, 0),
 	(5, 'L15320836', '434', NULL, NULL, 1, 1, 'Madelena', 'Priar', 'madelena@priar.com', '09038877951', 'Female', '1950-11-16', 0, 0);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
