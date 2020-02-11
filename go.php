abda03f0c4e9ed13b5e0657a95b84867fac6db32
Anthony O. Osawere <anthony@osawere.com>
2020-02-11 07:48:08 +0100

    Update go.php

--- go.php
+++ go.php
@@ -5,7 +5,20 @@ $oPage = oRoute::page();
 
 $app = new App();
 
+if(oMethod::isGet('MID')){
+	$MID = oData::oRead('MID', 'oGET');
+} elseif(oMethod::isPost('MID')){
+	$MID = oData::oRead('MID', 'oPOST');
+} else {
+	$MID = '';
+}
+echo $MID;
 if($oRefPage == 'login' && $oPage == 'go'){$app->login();}
 elseif($oRefPage == 'member-new' && $oPage == 'go'){$app->member_new();}
 elseif($oPage == 'members'){$members = $app->members();}
+// elseif($oPage == 'member'){$member = $app->member($_GET['ID']);}
+elseif($oPage == 'transaction-post'){
+	if(i)
+		$member = $app->member($_GET['ID']);
+}
 ?>
