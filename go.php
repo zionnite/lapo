29fc808144cd7e4834a9c7f93a6c7385e176cde6
Anthony O. Osawere <anthony@osawere.com>
2020-02-11 07:16:22 +0100

    Update go.php

--- go.php
+++ go.php
@@ -1,9 +1,11 @@
 <?php
 require 'zenq/init.php';
 $oRefPage = oRoute::ref();
+$oPage = oRoute::page();
 
 $app = new App();
 
-if($oRefPage == 'login'){$app->login();}
-elseif($oRefPage == 'member-new'){$app->member_new();}
+if($oRefPage == 'login' && $oPage == 'go'){$app->login();}
+elseif($oRefPage == 'member-new' && $oPage == 'go'){$app->member_new();}
+elseif($oPage == 'members'){$members = $app->members();}
 ?>
