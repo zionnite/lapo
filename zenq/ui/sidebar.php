7b6627174e10c8026364c58585fb5b838a8187ad
Anthony O. Osawere <anthony@osawere.com>
2020-02-11 02:45:08 +0100

    Update sidebar.php

--- zenq/ui/sidebar.php
+++ zenq/ui/sidebar.php
@@ -1,186 +1,178 @@
 <div class="sidebar">
 	<nav class="sidebar-nav">
 		<ul class="nav">
-			<li class="nav-item">
-				<a class="nav-link" href="index.html">
-					<i class="fas fa-home"></i> Dashboard
-				</a>
+			<li class="nav-item"><a class="nav-link" href="index.php"><i class="fas fa-home"></i>Dashboard</a></li>
+
+			<li class="nav-item nav-dropdown"><a class="nav-link nav-dropdown-toggle" href="#"><i class="fas fa-users-cog"></i>Members</a>
+				<ul class="nav-dropdown-items">
+					<li class="nav-item"><a class="nav-link" href="member-new.php"><i class="fas fa-user-plus"></i></i>New Member</a></li>
+					<li class="nav-item"><a class="nav-link" href="members.php"><i class="fas fa-users"></i>View All Members</a></li>
+					<!-- <li class="nav-item">
+						<a class="nav-link" href="member.php">
+							<i class="fas fa-dot-circle"></i>Member Profile
+						</a>
+					</li> -->
+					<!-- <li class="nav-item">
+						<a class="nav-link" href="contributions.php">
+							<i class="fas fa-dot-circle"></i>Contributions
+						</a>
+					</li> -->
+				</ul>
 			</li>
+
+
+
+
 			<li class="nav-item nav-dropdown">
 				<a class="nav-link nav-dropdown-toggle" href="#">
 					<i class="fas fa-address-book"></i>Account</a>
 					<ul class="nav-dropdown-items">
-						<li class="nav-item">
+						<!-- <li class="nav-item">
 							<a class="nav-link" href="member-new.php">
-								<i class="fas fa-dot-circle"></i> Create Account
+								<i class="fas fa-dot-circle"></i>Create Account
+							</a>
+						</li> -->
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								<i class="fas fa-dot-circle"></i>Savings
 							</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="#">
-								<i class="fas fa-dot-circle"></i> Savings
+								<i class="fas fa-dot-circle"></i>Dividends
 							</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="#">
-								<i class="fas fa-dot-circle"></i> Dividends
+								<i class="fas fa-dot-circle"></i>Loan Interest
 							</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="#">
-								<i class="fas fa-dot-circle"></i> Loan Interest
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								<i class="fas fa-dot-circle"></i> Staff Salary
+								<i class="fas fa-dot-circle"></i>Staff Salary
 							</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="view_user.html">
-								<i class="fas fa-dot-circle"></i> Sitting Allowance
+								<i class="fas fa-dot-circle"></i>Sitting Allowance
 							</a>
 						</li>
 					</ul>
 				</li>
+
 				<li class="nav-item nav-dropdown">
 					<a class="nav-link nav-dropdown-toggle" href="#">
-						<i class="fas fa-users"></i> Members</a>
+						<i class="fas fa-link"></i>Transaction</a>
 						<ul class="nav-dropdown-items">
-							<li class="nav-item">
-								<a class="nav-link" href="member-new.php">
-									<i class="fas fa-dot-circle"></i> New Member
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="member.php">
-									<i class="fas fa-dot-circle"></i> Member Profile
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="members.php">
-									<i class="fas fa-dot-circle"></i> View all Members
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="contributions.php">
-									<i class="fas fa-dot-circle"></i> Contributions
-								</a>
-							</li>
-						</ul>
-					</li>
-					<li class="nav-item nav-dropdown">
-						<a class="nav-link nav-dropdown-toggle" href="#">
-							<i class="fas fa-link"></i> Transaction</a>
-							<ul class="nav-dropdown-items">
-								<a class="nav-link" href="transaction-post.php">
-									<i class="fas fa-money-bill"></i> Post Transaction
-								</a>
-								<li class="nav-item nav-dropdown">
-									<a class="nav-link nav-dropdown-toggle" href="#">
-										<i class="fas fa-file-upload"></i> Bulk Upload</a>
-										<ul class="nav-dropdown-items">
-											<li class="nav-item">
-												<a class="nav-link" href="bulk-upload-borrower-csv.php">
-													<i class="fas fa-dot-circle"></i> Upload Borrowers from CSV/Excel
-												</a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="bulk-upload-borrower-csv.php">
-													<i class="fas fa-dot-circle"></i> Upload Loans from CSV/Excel
-												</a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="bulk-upload-borrower-csv.php">
-													<i class="fas fa-dot-circle"></i> Upload Repayments from CSV/Excel
-												</a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="bulk-upload-borrower-csv.php">
-													<i class="fas fa-dot-circle"></i> Upload Expenses from CSV/Excel
-												</a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="bulk-upload-borrower-csv.php">
-													<i class="fas fa-dot-circle"></i> Upload Savings Accounts from CSV/Excel
-												</a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="bulk-upload-borrower-csv.php">
-													<i class="fas fa-dot-circle"></i> Upload Savings Transactions from CSV/Excel
-												</a>
-											</li>
-										</ul>
-									</li>
-								</ul>
-							</li>
+							<a class="nav-link" href="transaction-post.php">
+								<i class="fas fa-money-bill"></i>Post Transaction
+							</a>
 							<li class="nav-item nav-dropdown">
 								<a class="nav-link nav-dropdown-toggle" href="#">
-									<i class="fas fa-balance-scale"></i> Loans</a>
+									<i class="fas fa-file-upload"></i>Bulk Upload</a>
 									<ul class="nav-dropdown-items">
 										<li class="nav-item">
-											<a class="nav-link" href="loan-prepare.php">
-												<i class="fas fa-dot-circle"></i> Prepare Loan
+											<a class="nav-link" href="bulk-upload-borrower-csv.php">
+												<i class="fas fa-dot-circle"></i>Upload Borrowers from CSV/Excel
 											</a>
 										</li>
 										<li class="nav-item">
-											<a class="nav-link" href="loans.php">
-												<i class="fas fa-dot-circle"></i> View Loans
+											<a class="nav-link" href="bulk-upload-borrower-csv.php">
+												<i class="fas fa-dot-circle"></i>Upload Loans from CSV/Excel
 											</a>
 										</li>
 										<li class="nav-item">
-											<a class="nav-link" href="loan-due.php">
-												<i class="fas fa-dot-circle"></i> Due Loans
+											<a class="nav-link" href="bulk-upload-borrower-csv.php">
+												<i class="fas fa-dot-circle"></i>Upload Repayments from CSV/Excel
 											</a>
 										</li>
 										<li class="nav-item">
-											<a class="nav-link" href="missed-replacement.php">
-												<i class="fas fa-dot-circle"></i> Missed Repayment
+											<a class="nav-link" href="bulk-upload-borrower-csv.php">
+												<i class="fas fa-dot-circle"></i>Upload Expenses from CSV/Excel
 											</a>
 										</li>
 										<li class="nav-item">
-											<a class="nav-link" href="loan-arrear.php">
-												<i class="fas fa-dot-circle"></i> Loans in Arrears
+											<a class="nav-link" href="bulk-upload-borrower-csv.php">
+												<i class="fas fa-dot-circle"></i>Upload Savings Accounts from CSV/Excel
 											</a>
 										</li>
 										<li class="nav-item">
-											<a class="nav-link" href="loan-calculator.php">
-												<i class="fas fa-dot-circle"></i> Loan Calculator
+											<a class="nav-link" href="bulk-upload-borrower-csv.php">
+												<i class="fas fa-dot-circle"></i>Upload Savings Transactions from CSV/Excel
 											</a>
 										</li>
 									</ul>
 								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="#">
-										<i class="fas fa-landmark"></i> Savings
-									</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="expenses.php">
-										<i class="fas fa-share"></i> Expenses
-									</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="report.php">
-										<i class="fas fa-flag"></i> Report
-									</a>
-								</li>
-								<li class="nav-item nav-dropdown">
-									<a class="nav-link nav-dropdown-toggle" href="#">
-										<i class="fas fa-briefcase"></i> Inventory</a>
-										<ul class="nav-dropdown-items">
-											<li class="nav-item">
-												<a class="nav-link" href="inventory-new.php">
-													<i class="fas fa-dot-circle"></i> Add Inventory
-												</a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="inventory.php">
-													<i class="fas fa-dot-circle"></i> View Inventory
-												</a>
-											</li>
-										</ul>
+							</ul>
+						</li>
+						<li class="nav-item nav-dropdown">
+							<a class="nav-link nav-dropdown-toggle" href="#">
+								<i class="fas fa-balance-scale"></i>Loans</a>
+								<ul class="nav-dropdown-items">
+									<li class="nav-item">
+										<a class="nav-link" href="loan-prepare.php">
+											<i class="fas fa-dot-circle"></i>Prepare Loan
+										</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="loans.php">
+											<i class="fas fa-dot-circle"></i>View Loans
+										</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="loan-due.php">
+											<i class="fas fa-dot-circle"></i>Due Loans
+										</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="missed-replacement.php">
+											<i class="fas fa-dot-circle"></i>Missed Repayment
+										</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="loan-arrear.php">
+											<i class="fas fa-dot-circle"></i>Loans in Arrears
+										</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="loan-calculator.php">
+											<i class="fas fa-dot-circle"></i>Loan Calculator
+										</a>
 									</li>
 								</ul>
-							</nav>
-							<button class="sidebar-minimizer brand-minimizer" type="button"></button>
-						</div>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">
+									<i class="fas fa-landmark"></i>Savings
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="expenses.php">
+									<i class="fas fa-share"></i>Expenses
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="report.php">
+									<i class="fas fa-flag"></i>Report
+								</a>
+							</li>
+							<li class="nav-item nav-dropdown">
+								<a class="nav-link nav-dropdown-toggle" href="#">
+									<i class="fas fa-briefcase"></i>Inventory</a>
+									<ul class="nav-dropdown-items">
+										<li class="nav-item">
+											<a class="nav-link" href="inventory-new.php">
+												<i class="fas fa-dot-circle"></i>Add Inventory
+											</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="inventory.php">
+												<i class="fas fa-dot-circle"></i>View Inventory
+											</a>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+						<button class="sidebar-minimizer brand-minimizer" type="button"></button>
+					</div>
