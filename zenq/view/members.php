1db85e3cb74b299617c87b13bba1c5925db4c44d
Anthony O. Osawere <anthony@osawere.com>
2020-02-11 07:16:50 +0100

    Update members.php

--- zenq/view/members.php
+++ zenq/view/members.php
@@ -6,57 +6,61 @@
                   		<!-- Breadcrumb Menu-->
                   		<li class="breadcrumb-menu d-md-down-none">
                   			<div class="btn-group" role="group" aria-label="Button group">
-                  				<a class="btn" href="#">
-                  					<i class="icon-speech"></i>
-                  				</a>
-                  				<a class="btn" href="./">
-                  					<i class="icon-graph"></i>  Dashboard</a>
-                  					<a class="btn" href="#">
-                  						<i class="icon-settings"></i>  Viewing All Member</a>
-                  					</div>
-                  				</li>
-                  			</ol>
-                  			<div class="container-fluid">
-                  				<div class="animated fadeIn">
-                  					<div class="row">
-                  						<div class="col-sm-12">
-                  							<div class="card text-white bg-white">
-                  								<div class="card-body">
-                  									<input class="form-control" id="company" type="text" placeholder="Search for...">
-                  									<input type="submit" value="Search" class="mt-3 btn btn-success" />
-                  								</div>
-                  							</div>
+                  				<a class="btn" href="#"><i class="icon-speech"></i></a>
+                  				<a class="btn" href="./"><i class="icon-graph"></i> Dashboard</a>
+                  				<a class="btn" href="#"><i class="icon-settings"></i> Viewing All Member</a>
+                  			</div>
+                  		</li>
+                  	</ol>
+                  	<div class="container-fluid">
+                  		<div class="animated fadeIn">
+                  			<div class="row">
+                  				<div class="col-sm-12">
+                  					<div class="card text-white bg-white">
+                  						<div class="card-body">
+                  							<input class="form-control" id="company" type="text" placeholder="Search for...">
+                  							<input type="submit" value="Search" class="mt-3 btn btn-success">
                   						</div>
-                  						<div class="col-sm-12">
-                  							<div class="card">
-                  								<div class="card-header">
-                  									<i class="fa fa-align-justify"></i> Showing all Member</div>
-                  									<div class="card-body">
-                  										<table class="table table-responsive-sm table-striped">
-                  											<thead>
+                  					</div>
+                  				</div>
+                  				<div class="col-sm-12">
+                  					<div class="card">
+                  						<div class="card-header">
+                  							<i class="fa fa-align-justify"></i> Showing all Member</div>
+                  							<div class="card-body">
+                  								<div class="table-responsive">
+                  									<table class="table table-sm table-bordered table-hover otable" id="dataTable" width="100%" cellspacing="0">
+                  										<thead>
+                  											<tr>
+                  												<th>ID</th>
+                  												<th>Name</th>
+                  												<th>Email Address</th>
+                  												<th>Phone Number</th>
+                  												<th>Gender</th>
+                  												<th>Savings</th>
+                  												<th>Loans</th>
+                  												<th colspan="2">MANAGE</th>
+                  											</tr>
+                  										</thead>
+                  										<tbody>
+                  											<?php $sn = 0; foreach ($members as $member){$sn++;?>
                   												<tr>
-                  													<th>ID</th>
-                  													<th>First Name</th>
-                  													<th>Last Name</th>
-                  													<th>Email Address</th>
-                  													<th>Phone Number</th>
-                  													<th>Gender</th>
-                  													<th>D.O.B</th>
-                  													<th></th>
-                  												</tr>
-                  											</thead>
-                  											<tbody>
-                  												<tr>
-                  													<td>122109911 </td>
-                  													<td>Stephen </td>
-                  													<td>Osawaru</td>
-                  													<td>email@email.com</td>
-                  													<td>1234xxxx22</td>
-                  													<td><span class="badge badge-success">Male</span></td>
-                  													<td>12/3/1990</td>
-                  													<td>
-                  														<div class="dropdown">
-                  															<a class="btn btn-danger dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  													<td align="center" style="width: 7%"><?php echo $member['MemberID'];?></td>
+                  													<td style="width: 20%"><?php echo $member['LastName'].' '.$member['FirstName'];?></td>
+                  													<td style="width: 20%"><?php echo $member['Email'];?></td>
+                  													<td style="width: 10%"><?php echo $member['Phone'];?></td>
+                  													<td style="width: 5%"><?php echo $member['Gender'];?></td>
+                  													<td align="right"><?php echo '₦'.number_format($member['Savings']);?></td>
+                  													<td align="right"><?php echo '₦'.number_format($member['Loans']);?></td>
+                  													<td align="center" style="width: 8%">
+                  														<a href="./?view=new-transaction&ID=<?php echo $customer['EUID'];?>" class="btn btn-sm btn-success" title="Post Transaction"><i class="fas fa-wallet"></i>Post</a>
+                  													</td>
+                  													<td align="center" style="width: 8%">
+                  														<a href="./?view=new-transaction&ID=<?php echo $customer['EUID'];?>" class="btn btn-sm btn-primary" title="Prepare Loan"><i class="fas fa-money-check-alt"></i>Loan</a>
+                  													</td>
+
+                  														<!-- <div class="dropdown">
+                  															<a class="btn btn-sm btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   																Action
                   															</a>
                   															<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
@@ -66,58 +70,37 @@
                   																<a class="dropdown-item" href="#">View Dividends</a>
                   																<a class="dropdown-item" href="#">View Profile</a>
                   															</div>
-                  														</div>
+                  														</div> -->
                   													</td>
                   												</tr>
-                  												<tr>
-                  													<td>112340009 </td>
-                  													<td>Tony </td>
-                  													<td>Osawere</td>
-                  													<td>email@email.com</td>
-                  													<td>1234xxxx22</td>
-                  													<td><span class="badge badge-danger">Female</span></td>
-                  													<td>12/3/1991</td>
-                  													<td>
-                  														<div class="dropdown">
-                  															<a class="btn btn-danger dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  																Action
-                  															</a>
-                  															<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  																<a class="dropdown-item" href="#">View Savings</a>
-                  																<a class="dropdown-item" href="#">View Loans</a>
-                  																<a class="dropdown-item" href="#">View Loans Interest</a>
-                  																<a class="dropdown-item" href="#">View Dividends</a>
-                  																<a class="dropdown-item" href="#">View Profile</a>
-                  															</div>
-                  														</div>
-                  													</td>
-                  												</tr>
-                  											</tbody>
-                  										</table>
-                  										<ul class="pagination">
-                  											<li class="page-item">
-                  												<a class="page-link" href="#">Prev</a>
-                  											</li>
-                  											<li class="page-item active">
-                  												<a class="page-link" href="#">1</a>
-                  											</li>
-                  											<li class="page-item">
-                  												<a class="page-link" href="#">2</a>
-                  											</li>
-                  											<li class="page-item">
-                  												<a class="page-link" href="#">3</a>
-                  											</li>
-                  											<li class="page-item">
-                  												<a class="page-link" href="#">4</a>
-                  											</li>
-                  											<li class="page-item">
-                  												<a class="page-link" href="#">Next</a>
-                  											</li>
-                  										</ul>
-                  									</div>
+                  											<?php }?>
+                  										</tbody>
+                  									</table>
                   								</div>
+                  								<ul class="pagination">
+                  									<li class="page-item">
+                  										<a class="page-link" href="#">Prev</a>
+                  									</li>
+                  									<li class="page-item active">
+                  										<a class="page-link" href="#">1</a>
+                  									</li>
+                  									<li class="page-item">
+                  										<a class="page-link" href="#">2</a>
+                  									</li>
+                  									<li class="page-item">
+                  										<a class="page-link" href="#">3</a>
+                  									</li>
+                  									<li class="page-item">
+                  										<a class="page-link" href="#">4</a>
+                  									</li>
+                  									<li class="page-item">
+                  										<a class="page-link" href="#">Next</a>
+                  									</li>
+                  								</ul>
                   							</div>
                   						</div>
                   					</div>
                   				</div>
-                  			</main>
+                  			</div>
+                  		</div>
+                  	</main>
