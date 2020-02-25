308552975f89e13a35f5330d5a144ca39e3806f2
Anthony O. Osawere <anthony@osawere.com>
2020-02-25 13:05:19 +0100

    Update go.php

--- go.php
+++ go.php
@@ -20,8 +20,12 @@ if(!empty($MID)){
 if($oRefPage == 'login' && $oPage == 'go'){$app->login();}
 elseif($oRefPage == 'member-new' && $oPage == 'go'){$app->member_new();}
 elseif($oPage == 'members'){$members = $app->members();}
+elseif($oPage == 'loan-view'){
+	$loanViewInterests = $app->loan_view('interest');
+	$loanViewPrincipals = $app->loan_view('principal');
+}
 // elseif($oPage == 'member'){$member = $app->member($_GET['ID']);}
 elseif($oRefPage == 'transaction-post' && $oPage == 'go'){$app->transaction_post();}
 elseif($oRefPage == 'loan-prepare' && $oPage == 'go'){$app->loan_prepare();}
 elseif($oRefPage == 'loan-payment' && $oPage == 'go'){$app->loan_payment();}
-?> 
+?>
